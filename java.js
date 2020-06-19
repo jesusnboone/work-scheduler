48947fc3cb813deaca1cc3f12e8a078b8140faa6
jesusnboone <jesusnboone@gmail.com>
2020-06-19 02:59:10 -0500

    progress save, working on classes

--- java.js
+++ java.js
@@ -1,4 +1,11 @@
 var today = moment().format('dddd, MMMM Do YYYY');
-var hour 
+var hour = moment().startOf('hour');
+var eightAM = moment().startOf('day').add(8, 'hours').endOf('hour');
+console.log(eightAM);
+console.log(hour);
 
-document.getElementById("currentDay").innerHTML = today
+$("currentDay").innerHTML = today
+
+if (moment().isAfter(eightAM)) {
+    $("#eight-am").addClass("past");
+}
